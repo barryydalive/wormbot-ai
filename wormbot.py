937885d6f13874e7e320eb2610c4abc490eacb8a
barryydalive <barry.huang93@gmail.com>
2024-01-30 15:42:11 -0500

    dotenv

--- wormbot.py
+++ wormbot.py
@@ -1,8 +1,15 @@
 import discord
 import openai
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+DISCORD_TOKEN = os.getenv('DISCORD_TOKEN')
+OPENAI_API_KEY = os.getenv('OPENAI_API_KEY')
 
 # Set up your OpenAI API key
-openai.api_key = 'YOUR_OPENAI_API_KEY'
+openai.api_key = OPENAI_API_KEY
 
 # Set up your Discord bot client
 client = discord.Client()
@@ -26,4 +33,4 @@ async def on_message(message):
     await message.channel.send(response.choices[0].text.strip())
 
 # Run your bot with the Discord token
-client.run('YOUR_DISCORD_BOT_TOKEN')
+client.run(DISCORD_TOKEN)
